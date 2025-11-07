cd6e23a5ede74940f07e14cfbed2a6561efe8a84
voxdigitalhoy-cmd <voxdigitalhoy@gmail.com>
2025-11-07 04:39:27 -0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,4 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  root: "./",
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    port: 3000,
+  },
 });
