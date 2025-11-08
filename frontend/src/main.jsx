0758859192038ede331e3a49edccbff6ae52bc15
voxdigitalhoy-cmd <voxdigitalhoy@gmail.com>
2025-11-07 18:36:04 -0600

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css"; // CSS global (puede estar vacío o con estilos básicos)
+import App from "./App";
+import "./index.css"; // Asegúrate que este archivo exista
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
